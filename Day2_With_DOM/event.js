6a989ee1ee289c176adb67ba5f812e5680f5d8fe
katha922 <kathasaha922@gmail.com>
2024-11-02 22:23:46 +0600

    add onclick handler

---
+++ Day2_With_DOM/event.js
@@ -0,0 +1,8 @@
+const makeRedButton = document.getElementById("btn");
+makeRedButton.onclick=makeRed;
+function makeRed(){
+    document.body.style.backgroundColor='red'
+}
+function makeBlue(){
+    document.body.style.backgroundColor='blue'
+}
