28420d0449d4e3471bf5d967345aa4b360ffa88e
katha922 <kathasaha922@gmail.com>
2024-11-02 23:30:08 +0600

    addEventListener

--- Day2_With_DOM/event.js
+++ Day2_With_DOM/event.js
@@ -1,8 +1,24 @@
+//option-1
 const makeRedButton = document.getElementById("btn");
 makeRedButton.onclick=makeRed;
 function makeRed(){
     document.body.style.backgroundColor='red'
 }
+//option-2
 function makeBlue(){
     document.body.style.backgroundColor='blue'
 }
+//option-3
+const greenButton = document.getElementById('make-green');
+greenButton.onclick=function makeGreen(){
+    document.body.style.backgroundColor='green'
+}
+//option-4
+const purpleButton = document.getElementById('make-purple');
+purpleButton.addEventListener('click', function makePurple(){
+    document.body.style.backgroundColor= 'purple'
+})
+//option-5 important
+document.getElementById('make-orange').addEventListener('click',function(){
+    document.body.style.backgroundColor='orange';
+})
