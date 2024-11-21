3ac51cfea61eb79f205a10dbe1dd8eaa8c5f64f6
katha922 <kathasaha922@gmail.com>
2024-11-21 19:41:26 +0600

    added condition based on click and used conditional css

--- Day21/src/components/Country/Country.jsx
+++ Day21/src/components/Country/Country.jsx
@@ -1,11 +1,21 @@
 import './Country.css'
+import { useState } from "react";
 
 const Country = ({country}) => {
-    const {name,flags} = country;
+    const {name,flags,area,population,cca3} = country;
+    const[visited,setVisited]=useState(false)
+    const handleVisited=()=>{
+        setVisited(!visited)
+    }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'visited'}`}>
             <img src={flags?.png} alt="" />
             <h3>Name: {name?.common}</h3>
+            <p>Area: {area}</p>
+            <p>Population: {population}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisited} className='btn-1'>{visited?'Visited':'Going'}</button>
+            {/* {visited ? 'i have visited' : 'i want to visit'} */}
             
         </div>
     );
