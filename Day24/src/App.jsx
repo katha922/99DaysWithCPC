98880d5d7dda01b586cd7bfd6ae9d6e5ee212840
katha922 <kathasaha922@gmail.com>
2024-11-24 22:03:48 +0600

    added hero section

--- Day24/src/App.jsx
+++ Day24/src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import './App.css'
+import Hero from './components/Navbar/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Hero></Hero>
     </>
   )
 }
---
+++ Day24/src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+
+
+const Hero = () => {
+    return (
+        <div>
+           <div className="text-center m-20">
+            <p className="text-blue-500">Your Vision, Our Code – Let's Build Together.</p>
+            <h1 className="text-3xl m-2 font-bold">Next-Level Websites for Next-Level Brands</h1>
+            <h3 className="text-xl font-semibold">Web Solutions, Redefined</h3>
+            <button className="mt-10 border-blue-500 border-2 rounded p-2 font-semibold ">GET STARTED</button>
+           </div>
+        </div>
+    );
+};
+
+export default Hero;
